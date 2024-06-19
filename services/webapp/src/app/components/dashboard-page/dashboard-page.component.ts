e9b7c9982b1cd7790ed6cfa57ae7f5f8a38dc043
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2024-06-19 16:23:21 -0400

    feat: added structure for dashboard

--- services/webapp/src/app/components/dashboard-page/dashboard-page.component.ts
+++ services/webapp/src/app/components/dashboard-page/dashboard-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -6,19 +7,28 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./dashboard-page.component.css']
 })
 export class DashboardPageComponent {
-  @Input() taskName: string = 'TASK_NAME';
-  @Input() questions: { corrected: number, total: number }[] = [
+  taskName: string = 'Tâche';
+  questions: { corrected: number, total: number }[] = [
     { corrected: 0, total: 150 },
     { corrected: 10, total: 150 },
     { corrected: 75, total: 150 }
   ];
-  @Input() averages: string[] = [
+  averages: string[] = [
     '0/5',
     '2.18/3',
     '3.47/5'
   ];
+  constructor(private router: Router) { }
 
   correctQuestion() {
     console.log('Correction button clicked');
+  }
+
+  shareQuestion(index: number) {
+    console.log(`Share question ${index + 1}`);
+  }
+
+  reroute() {
+    this.router.navigate(['/tasks-history']);
   }
 }
