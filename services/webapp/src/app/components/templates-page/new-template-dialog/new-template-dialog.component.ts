6e2bdcf88218f520b80af02644aea2e606ff2ec6
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2024-06-25 15:01:22 -0400

    feat: Add drag and drop functionality to new-template-dialog component

--- services/webapp/src/app/components/templates-page/new-template-dialog/new-template-dialog.component.ts
+++ services/webapp/src/app/components/templates-page/new-template-dialog/new-template-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatDialogRef } from '@angular/material/dialog';
 import { TemplateService } from 'src/app/services/template.service';
@@ -26,18 +26,17 @@ export class NewTemplateDialogComponent implements OnInit {
   CopyFileEvent(fileInput: Event){
     let target= fileInput.target as HTMLInputElement;
     let file: File = (target.files as FileList)[0];
-    this.copy = file;
-    this.copyName = file.name;
-
-    this.checkDisabled();
+    this.setCopy(file);
   }
   
+  setCopy(file: File) {
+    this.copy = file;
+    this.copyName = file.name;
+    this.checkDisabled();
+  }
+
   checkDisabled(){
-    if (this.copyName !== ""){
-      this.disabled = false;
-    }else{
-      this.disabled = true;
-    }
+    this.disabled = !this.copyName;
   }
 
   async confirm() {
@@ -48,4 +47,20 @@ export class NewTemplateDialogComponent implements OnInit {
     this.router.navigate(['/template-editor']);
   }
 
+  @HostListener('dragover', ['$event']) 
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  @HostListener('drop', ['$event']) 
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (event.dataTransfer && event.dataTransfer.files.length > 0) {
+      const file = event.dataTransfer.files[0];
+      this.setCopy(file);
+      event.dataTransfer.clearData();
+    }
+  }
 }
