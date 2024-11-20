f2c235a8627c106c8fc14bb80b7ef61f61064302
legraina <antoine.legrain@gmail.com>
2024-11-20 10:54:56 -0500

    Fixed sacle factor for the eraser.

--- services/webapp/ng/src/app/components/pdf-viewer/pdf-viewer.component.ts
+++ services/webapp/ng/src/app/components/pdf-viewer/pdf-viewer.component.ts
@@ -710,7 +710,7 @@ export class PDFViewerComponent implements OnInit, OnChanges, OnDestroy {
     // remove parts of the points => transform it in several paths
     const newPaths: BezierPath[] = [];
     let newPath: BezierPath = new BezierPath();
-    let bezierIndex = 0, radius2 = Math.pow(this.radius, 2);
+    let bezierIndex = 0, radius2 = Math.pow(this.radius / this.getScaleFactor(), 2);
     let modified = false;
     for (let [x, y] of path.points) {
       // keep this (x,y) if far enough from center
