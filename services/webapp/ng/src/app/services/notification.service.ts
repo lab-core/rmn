e9970abe9211847f8ff7294403b214786fc26b5b
legraina <antoine.legrain@gmail.com>
2025-10-23 01:50:07 -0400

    When uploading grade, check if a valid doc_index before trying to modifify the associated exam.

--- services/webapp/ng/src/app/services/notification.service.ts
+++ services/webapp/ng/src/app/services/notification.service.ts
@@ -2,26 +2,30 @@ import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationService {
 
-  constructor(private toastr: ToastrService) { }
-
-  showSuccess(message, title, options: any = {}){
-      this.toastr.success(message, title, options)
+  constructor(private toastr: ToastrService) {
   }
 
-  showError(message, title, options: any = {}){
-      this.toastr.error(message, title, options)
+  showSuccess(message, title, options: any = {}) {
+    console.log('Success Notification:', title, message);
+    this.toastr.success(message, title, options);
   }
 
-  showInfo(message, title, options: any = {}){
-      this.toastr.info(message, title, options)
+  showError(message, title, options: any = {}) {
+    console.log('Error Notification:', title, message);
+    this.toastr.error(message, title, options);
   }
 
-  showWarning(message, title, options: any = {}){
-      this.toastr.warning(message, title, options)
+  showInfo(message, title, options: any = {}) {
+    console.log('Info Notification:', title, message);
+    this.toastr.info(message, title, options);
   }
 
+  showWarning(message, title, options: any = {}) {
+    console.log('Warning Notification:', title, message);
+    this.toastr.warning(message, title, options);
+  }
 }
